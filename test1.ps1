609dc9dc3d7f9999e5ab7220a3b8b6f5da1e9108
Charles ABELARD <charles.abelard1@gmail.com>
2022-02-24 20:06:12 +0100

    add test1

---
+++ test1.ps1
@@ -0,0 +1,11 @@
+﻿# $File[0] nbr projets nbr contrib
+$NbrProjets = $File[0].Split(" ")[0]
+$NbrColab = $File[0].Split(" ")[1]
+$ligne = 1
+$Colab1 = $File[$ligne]
+$ligne ++
+
+$colab1Name = $Colab1.Split(" ")[0]
+$colab1nbrCompetance = $Colab1.Split(" ")[1]
+
+$colabComp = $File[$ligne]
